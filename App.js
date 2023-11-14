ef15980d0d9932f7d04858a8f74a60a89e30da61
matf8 <mathias.lfernandez@gmail.com>
2023-11-14 12:52:25 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [token, setToken] = useState('');
+
+	useEffect(() => {
+		registerForPushNotificationsAsync().then((token) => {
+			setToken(token);
+		});
+	}, []);
+
+	registerForPushNotificationsAsync = async () => {
+		let token;
+		if (Device.isDevice) {
+			const { status: existingStatus } = await Notifications.getPermissionsAsync();
+			let finalStatus = existingStatus;
+			if (existingStatus !== 'granted') {
+				const { status } = await Notifications.requestPermissionsAsync();
+				finalStatus = status;
+			}
+			if (finalStatus !== 'granted') {
+				alert('Failed to get push token for push notification!');
+				return;
+			}
+			token = (await Notifications.getDevicePushTokenAsync()).data;
+		} else {
+			alert('Must use physical device for Push Notifications');
+		}
+		if (Platform.OS === 'android') {
+			Notifications.setNotificationChannelAsync('default', {
+				name: 'default',
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: '#FF231F7C',
+			});
+		}
+		return token;
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text>Push Notifications {token}</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+
